387f3298c5b982a09df72f52e793b6f66c280e43
Abhinav Singh yadav <resoabhisingh@gmail.com>
2026-04-21 17:21:42 +0530

    cross enable

--- src/main/java/com/demo/demo/controller/Crudclass.java
+++ src/main/java/com/demo/demo/controller/Crudclass.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.demo.demo.entity.Student;
 import com.demo.demo.services.Crudservice;
 
+@CrossOrigin(origins = "*")    
 @RestController
 @RequestMapping("/api/student")
 public class Crudclass {
